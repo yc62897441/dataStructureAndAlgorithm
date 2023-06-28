10d0252a66c87a78c14aa1400bf8c6e5aef93c66
unknown <FP0023@FoxwellPower.Local>
2023-06-28 13:43:23 +0800

    chapter12 LCS 更新

--- chapter12/147 Intro to LCS/myAnswer.js
+++ chapter12/147 Intro to LCS/myAnswer.js
@@ -1,18 +1,20 @@
 // 我自己想的
 let str1 = 'ATAACGCGCTGCTCGGGT'
 let str2 = 'TCAATCAGGATCCGCTGA'
+let count1 = 0
 
 function lcs(str1, str2) {
     let currentLcs = '' // 當前找到最長的結果
 
-    // str1 當前要比對的字元起始位置 i。如果起始位置之後所剩的長度 < currentLcs.length 代表就不用繼續找了。
-    for (let i = 0; i < str1.length - 1 - currentLcs.length; i++) {
+    // str1 當前要比對的字元起始位置 i。
+    for (let i = 0; i < str1.length; i++) {
         let tempLcs = ''
         let startIndex = i
 
-        // str2 當前要比對的字元起始位置 j。如果起始位置之後所剩的長度 < currentLcs.length 代表就不用繼續找了。
-        for (let j = 0; j < str2.length - 1 - currentLcs.length; j++) {
+        // str2 當前要比對的字元起始位置 j。
+        for (let j = 0; j < str2.length; j++) {
             let currentLetter = str1[startIndex]
+            count1++
 
             if (currentLetter === str2[j]) {
                 tempLcs = tempLcs + currentLetter
@@ -29,3 +31,24 @@ function lcs(str1, str2) {
 }
 
 console.log(lcs(str1, str2))
+console.log(count1)
+
+let count2 = 0
+function aa(str1, str2) {
+    count2++
+    if (str1.length === 0 || str2.length === 0) {
+        return 0
+    }
+
+    if (str1[str1.length - 1] === str2[str2.length - 1]) {
+        return 1 + aa(str1.substring(0, str1.length - 1), str2.substring(0, str2.length - 1))
+    } else {
+        return Math.max(
+            aa(str1.substring(0, str1.length), str2.substring(0, str2.length - 1)),
+            aa(str1.substring(0, str1.length - 1), str2.substring(0, str2.length))
+        )
+    }
+}
+
+console.log(aa(str1, str2))
+console.log(count2)
