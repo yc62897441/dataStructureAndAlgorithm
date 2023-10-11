8b10e1ac85b607e1adfd21fbd01d6bb685c3668a
unknown <yc62897441@gmail.com>
2023-10-11 11:17:41 +0800

    feat: 更新 chapter4

--- chapter4/33 Unique Letter Substring/myAnswer.js
+++ chapter4/33 Unique Letter Substring/myAnswer.js
@@ -25,35 +25,37 @@ function uniqueLetterString(str) {
     }
     return maxLength
 }
-// console.log(uniqueLetterString('thisishowwedoit'))
-// console.log(uniqueLetterString('abccbazazbcdefefdcba'))
+console.log(uniqueLetterString('thisishowwedoit')) // 6
+console.log(uniqueLetterString('abccbazazbcdefefdcba')) // 7
 
 // 教案解法
 // function uniqueLetterString(str) {
-//     let start = 0;
-//     let end = 0;
-//     let counter = {};
-//     let maxLength = -Infinity;
-  
+//     let start = 0
+//     let end = 0
+//     let counter = {}
+//     let maxLength = -Infinity
+
 //     while (end < str.length) {
-//       console.log(counter);
-//       if (counter[str[end]]) {
-//         counter[str[start]]--;
-//         start++;
-//       } else {
-//         counter[str[end]] = 1;
-//         end++;
-//         if (end - start > maxLength) {
-//           maxLength = end - start;
+//         // console.log(counter)
+//         if (counter[str[end]]) {
+//             counter[str[start]]--
+//             start++
+//         } else {
+//             counter[str[end]] = 1
+//             end++
+//             if (end - start > maxLength) {
+//                 maxLength = end - start
+//             }
 //         }
-//       }
 //     }
-  
+
 //     if (maxLength == -Infinity) {
-//       console.log("Cannot find unique letters substring.");
-//       return null;
+//         console.log('Cannot find unique letters substring.')
+//         return null
 //     }
-  
-//     console.log(maxLength);
-//     return maxLength;
-//   }
+
+//     console.log(maxLength)
+//     return maxLength
+// }
+// uniqueLetterString('thisishowwedoit')
+// uniqueLetterString('abccbazazbcdefefdcba')
--- chapter4/35 Recursion/myAnswer.js
+++ chapter4/35 Recursion/myAnswer.js
@@ -2,14 +2,13 @@
 // 給定一個 n(正整數)，寫出一個 function 可以回傳數列 T 第 n 項的值，如果 T(1) = 10; T(2) = T(1) + 15; T(n) = T(n - 1) + 15
 
 // 我的解答
-function aa(n) {
+function findSequence(n) {
     if (n === 1) {
         return 10
     }
-    return aa(n - 1) + 15 
+    return findSequence(n - 1) + 15
 }
 
-console.log(aa(1))
-console.log(aa(2))
-console.log(aa(3))
-
+console.log(findSequence(1))
+console.log(findSequence(2))
+console.log(findSequence(3))
